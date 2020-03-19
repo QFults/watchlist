d1caedb14ef4bf92ac3405692b337acc32da8d7e
Quinton Fults <coding.banjo@gmail.com>
2020-03-19 14:03:33 -0700

    Updates

---
+++ precache-manifest.7d0fa30a3177edab0a26729c730eed39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "708f5ee6201d465f41499d521348a66f",
+    "url": "/watchlist/index.html"
+  },
+  {
+    "revision": "4644076165572c678e0b",
+    "url": "/watchlist/static/js/2.033de941.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/watchlist/static/js/2.033de941.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b539a5b7b192978b2cce",
+    "url": "/watchlist/static/js/main.93e2cca6.chunk.js"
+  },
+  {
+    "revision": "94b3373fa089c608da7d",
+    "url": "/watchlist/static/js/runtime-main.715de581.js"
+  }
+]);
